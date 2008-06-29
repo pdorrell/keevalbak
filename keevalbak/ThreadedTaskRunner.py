44f656ee35c45c11ae8cba269abafa918a4f6b8a
Philip Dorrell <http://www.1729.com/email.html>
2008-06-29 20:42:32 +1200

    dont log thread start/finish

--- keevalbak/ThreadedTaskRunner.py
+++ keevalbak/ThreadedTaskRunner.py
@@ -12,7 +12,7 @@ class TaskProcessor(threading.Thread):
     def run(self):
         while True:
             task = self.queue.get()
-            print "Thread %d performing task ..." % self.index
+            #print "Thread %d performing task ..." % self.index
             for attribute in self.clonedAttributes:
                 if not self.clonedAttributesMap.has_key (attribute):
                     attributeValue = task.__dict__[attribute].clone()
@@ -20,7 +20,7 @@ class TaskProcessor(threading.Thread):
                 task.__dict__[attribute] = self.clonedAttributesMap[attribute]
             task.doUnsynchronized()
             self.queue.task_done()
-            print "Thread %d finished performing task ..." % self.index
+            #print "Thread %d finished performing task ..." % self.index
             
 class ThreadedTaskRunner:
     def __init__(self, numThreads = 10, clonedAttributes = []):
